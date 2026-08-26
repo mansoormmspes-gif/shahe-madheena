0fde099dec38068683a60fcdde966bf3dc2ac767
Muhammed Mansoor <mansoormmspes@gmail.com>
2026-08-26 10:23:38 +0530

    chore: remove helper scripts

--- fix_err.js
+++
@@ -1,6 +0,0 @@
-﻿
-const fs = require("fs");
-let content = fs.readFileSync("src/app/admin/results/page.tsx", "utf-8");
-content = content.replace(/catch \(err\) \{/g, "catch (err: any) {");
-fs.writeFileSync("src/app/admin/results/page.tsx", content, "utf-8");
-
